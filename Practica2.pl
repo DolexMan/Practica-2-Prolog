db11ec3d6800cf04f1445fae31ced8262e1eabe5
Anfereza29 <130261734+Anfereza29@users.noreply.github.com>
2025-10-01 03:52:26 +0000

    Hechos subidos y preparados para las funciones

--- Practica2.pl
+++ Practica2.pl
@@ -1,1 +1,29 @@
-decirHolamundo :- write('Hola Mundo').
+/******************************************************************************
+
+*******************************************************************************/
+conexion(madrid, barcelona).
+conexion(madrid, paris).
+conexion(roma, berlin).
+conexion(ibiza, paris).
+conexion(ibiza, monaco).
+conexion(monaco, madrid).
+
+intereses(paris,iglesia).
+
+diaAbierto(lunes,[madrid,barcelona,roma,berlin,ibiza,monaco]).
+diaAbierto(martes,[madrid,barcelona,roma,ibiza,monaco]).
+diaAbierto(miercoles,[madrid,barcelona,paris,roma,berlin,monaco]).
+diaAbierto(jueves,[madrid,barcelona,paris,roma]).
+diaAbierto(viernes,[madrid,barcelona,paris,roma,berlin,ibiza,monaco]).
+diaAbierto(sabado,[barcelona,paris,berlin,ibiza]).
+diaAbierto(domingo,[paris,monaco]).
+
+
+
+main:-
+        process,
+        halt.
+
+process:-
+        write('Hello World').
+:- main.
